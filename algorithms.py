7c967c8fed41b9c3195e233c537c51c0dc9d9d7c
mohamadbaqer7 <mohamadbakerissa88@gmail.com>
2025-10-06 10:56:18 +0300

    Add find max and linear search

--- algorithms.py
+++ algorithms.py
@@ -1,8 +1,19 @@
 def factorial(n):  # O(n)
     if n < 0:
         return "Error: negative number"
-    r = 1
+    x = 1
     for i in range(2, n + 1):
-        r *= i
-    return r
+        x *= i
+    return x
 
+def find_max(list):  # O(n)
+    if not list:
+        return "Error: empty list"
+    max = list[0]
+    for x in list[1:]:
+        if x > max:
+            max = x
+    return max
+
+def linear_search(list, t):  # O(n)
+    return t in list
