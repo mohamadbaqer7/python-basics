eb0af86838b1bb00d212aeb8bf0d84bd1f8677a5
mohamadbaqer7 <mohamadbakerissa88@gmail.com>
2025-10-06 08:20:16 +0300

    Add info.py file

---
+++ info.py
@@ -0,0 +1,4 @@
+print("Mouhamad baqer issa")
+print("""I want to learn programming because it is a way to communicate with the computer 
+      and because it is one of the most important fields in this time,
+      which develops many of my skills""")
